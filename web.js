414764177d341e4c1f2e8572bb899974cd787068
Prish <prishantha@gmail.com>
2013-07-13 00:26:02 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-#var output = buf.toString(fs.readFileSync(index.html));
+//var output = buf.toString(fs.readFileSync(index.html));
 
 app.get('/', function(request, response) {
   response.send("Hello Worldsss!");
